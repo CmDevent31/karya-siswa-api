c61680159db8ff912710a5f7603847da7b56c644
CmDevent31 <malkysudrajat31@gmail.com>
2023-08-25 16:31:07 +0700

    memulai repositori baru

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DataController;
-use App\Http\Controllers\GuruController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\EkskulController;
-use App\Http\Controllers\QuotesController;
-use App\Http\Controllers\TimKamiController;
-use App\Http\Controllers\FAQSHelpController;
-use App\Http\Controllers\PrestasiController;
-use App\Http\Controllers\TestimoniController;
-use App\Http\Controllers\KaryaSiswaController;
-use App\Http\Controllers\RegistrasiController;
-use App\Http\Controllers\TentangKamiController;
-use App\Http\Controllers\PageNotFoundController;
-use App\Http\Controllers\PembelajaranController;
-use App\Http\Controllers\GaleriSekolahController;
-use App\Http\Controllers\PrivacyPolicyController;
-use App\Http\Controllers\TermsConditionController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -34,27 +17,6 @@ use App\Http\Controllers\TermsConditionController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/Home', [HomeController::class, 'index']);
-Route::get('/TentangKami', [TentangKamiController::class, 'index']);
-Route::get('/Pembelajaran', [PembelajaranController::class, 'index']);
-Route::get('/TimKami', [TimKamiController::class, 'index']);
-Route::get('/PageNotFound', [PageNotFoundController::class, 'index']);
-Route::get('/Login', [LoginController ::class, 'index']);
-Route::get('/Registrasi', [RegistrasiController ::class, 'index']);
-Route::get('/Guru', [GuruController ::class, 'index']);
-Route::get('/Prestasi', [PrestasiController ::class, 'index']);
-Route::get('/GaleriSekolah', [GaleriSekolahController ::class, 'index']);
-Route::get('/KaryaSiswa', [KaryaSiswaController ::class, 'index']);
-Route::get('/PrivacyPolicy', [PrivacyPolicyController ::class, 'index']);
-Route::get('/TermsCondition', [TermsConditionController ::class, 'index']);
-Route::get('/FaqsHelp', [FAQSHelpController ::class, 'index']);
-Route::get('/Quotes', [QuotesController ::class, 'index']);
-Route::get('/data',  [DataController ::class, 'index']);
-
-
-Route::get('/Ekskul', [EkskulController::class, 'index']);
-Route::get('/PageNotFound', [PageNotFoundController::class, 'index']);
 
 
 
